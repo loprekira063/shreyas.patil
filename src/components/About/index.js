b6ea5225e58cc9318ed4686283ffd06ee06fa6ad
unknown <joshianushka004@gmail.com>
2024-06-18 08:17:37 +0530

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+           Currently a student at Pimpri Chinchwad College Of Engineering and Research in Pune, pursuing my passion for technology and software development. Proficient in Java, React, HTML, CSS, JavaScript, Tailwind CSS, C, and C++, I am eager to explore new technologies and frameworks. At 21 years old, I am driven by a strong interest in leveraging cutting-edge technologies and methodologies to create innovative solutions. My goal is to apply my skills and enthusiasm to secure a rewarding position in the tech industry.
+          </p>
+          <p align="LEFT">
+          I am a naturally curious individual with a quiet confidence, constantly refining my skills through each design challenge I tackle. If I were to encapsulate myself in one sentence, I would describe myself as an esports enthusiast, passionate about trekking, and thoroughly obsessed with technology. My interests extend beyond technology to include exploring the competitive world of esports and embarking on thrilling treks.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
